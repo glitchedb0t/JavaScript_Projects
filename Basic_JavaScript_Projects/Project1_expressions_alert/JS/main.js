0416bcc48594d513da52346d5d44b8f6b490c2f6
Meg Lee <megclee88@gmail.com>
2023-05-15 18:26:33 -0700

    Basic JavaScript Projects submission assignment

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+// Creating two variables - sent1 and sent2
+var sent1 = ("This is the beginning of the string,"), sent2 = (" and this is the end of the string.")
+
+// Concatenating sent1 and sent2 variables together
+document.write(sent1 + sent2);
+
+// Writing an expression
+window.alert(10 / 5);
+
+// Both document.write() and window.alert() used above.
+// main.js is linked in the index.html file.
