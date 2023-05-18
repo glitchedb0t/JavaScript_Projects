d1dd16a8becc44d48744583cc71825da7844e453
Meg Lee <megclee88@gmail.com>
2023-05-17 21:48:53 -0700

    Project4_dictionaries submission assignment

---
+++ Basic_JavaScript_Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,18 @@
+// This function creates a dictionary of ice cream flavors by color
+function my_Dictionary() {
+    var iceCreamFlavors = {
+        Brown: "Chocolate",
+        White: "Vanilla",
+        Pink: "Strawberry",
+        Green: "Pistachio",
+        Orange: "Creamsicle",
+        Yellow: "Lemon",
+        Blue: "Bubblegum"
+    }
+
+    // This line of code deletes the key Pink which removes the value Strawberry from the dictionary
+    delete iceCreamFlavors.Pink;
+
+    // This line of code identifies the HTML element id'ed by dictionary and outputs the value paired with the Yellow key
+    document.getElementById("dictionary").innerHTML = iceCreamFlavors.Yellow;
+}
